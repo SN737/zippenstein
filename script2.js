77d6e121812464ba6a36b6de052e388d25d63199
Sergey <92508690+SN737@users.noreply.github.com>
2022-07-23 16:11:27 +0300

    v1
    v1

---
+++ script2.js
@@ -0,0 +1,11 @@
+
+let push = document.querySelector('.butt');
+let wrap1 = document.querySelector('.wrap');
+
+
+push.addEventListener('click', () => {
+    console.log(';eukmbt ');
+    const zipp = document.createElement('div');
+    zipp.classList.add('zipper');
+    wrap1.append(zipp);
+})
